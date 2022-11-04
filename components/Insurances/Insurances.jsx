573dee5cf87c86f956af712df7f87f1218bc722e
sedhha <activity.schoolsh2@gmail.com>
2022-11-04 15:05:56 +0530

    Added Component did mount logic

--- components/Insurances/Insurances.jsx
+++ components/Insurances/Insurances.jsx
@@ -2,17 +2,22 @@ import React from 'react'
 import classes from "./Insurances.module.css";
 import InsuranceCard from "./InsuranceCard/Card";
 import FullWidthSpinner from '../Utils/Spinner/FullWidthSpinner';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Popup from '../Utils/Popup/Popup';
 import Terms from './Terms/Terms';
 import Purchase from './Purchase/Purchase';
 import PurchaseSuccess from "./Purchase/PurchaseSuccess"
+import { fetchOffersByFilter } from '../../redux/slices/filter.slices';
 
 
 export default function Insurances() {
     const { loading } = useSelector(state => state.filter)
     const { cards } = useSelector(state => state.cards)
     const { showTerms, showPurchase, purchaseSuccess } = useSelector(state => state.custom)
+    const dispatch = useDispatch()
+    React.useEffect(() => {
+        dispatch(fetchOffersByFilter())
+    }, [])
     return (
         <div className={classes.Container}>
             {loading && <FullWidthSpinner />}
